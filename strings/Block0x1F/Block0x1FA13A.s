060ab247fd63cabacb953df8b583a43a3d1e3584
JohnTsq <msn451518580@hotmail.com>
2024-10-29 22:41:38 +0800

    新增导出文本、清理
    导出-文本-库洛牌介绍
    清理临时文件

---
+++ strings/Block0x1F/Block0x1FA13A.s
@@ -0,0 +1,448 @@
+;Block0x1FA13A
+;子文件文本，共25条子文本
+.org 0x1F9C86   ;可自定义文本地址
+.area 0x1FA13A-.,0xFF   ;原位覆写须限定area
+
+.align 2 :: Block0x1FA13A_000:
+;－－－－－－－－－－－－
+;.strn "ウィンテﾞィ{换行}"
+;.strn "　４大けﾞんそのひ{换行}"
+;.strn "とつ､風をつかさ{换行}"
+;.strn "とﾞる｡風てﾞ相手を{换行}"
+;.strn "しはﾞりつけて攻撃{换行}"
+;.strn "する｡{结束}"
+;－－－－－－－－－－－－
+ .strn "ウィンテﾞィ{换行}"
+ .strn "　４大けﾞんそのひ{换行}"
+ .strn "とつ､風をつかさ{换行}"
+ .strn "とﾞる｡風てﾞ相手を{换行}"
+ .strn "しはﾞりつけて攻撃{换行}"
+ .strn "する｡{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2 :: Block0x1FA13A_001:
+;－－－－－－－－－－－－
+;.strn "フライ{换行}"
+;.strn "　魔法の杖にはね{换行}"
+;.strn "をはやして空をと{换行}"
+;.strn "ふﾞ｡敵の攻撃かﾞよ{换行}"
+;.strn "けやすくなる｡{换行}"
+;.strn "　１０ターンのみ｡{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "フライ{换行}"
+ .strn "　魔法の杖にはね{换行}"
+ .strn "をはやして空をと{换行}"
+ .strn "ふﾞ｡敵の攻撃かﾞよ{换行}"
+ .strn "けやすくなる｡{换行}"
+ .strn "　１０ターンのみ｡{结束}{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2 :: Block0x1FA13A_002:
+;－－－－－－－－－－－－
+;.strn "シャトﾞウ{换行}"
+;.strn "　大きく真っくろ{换行}"
+;.strn "なかけﾞてﾞ相手をつ{换行}"
+;.strn "つみこんてﾞ攻撃す{换行}"
+;.strn "る｡{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "シャトﾞウ{换行}"
+ .strn "　大きく真っくろ{换行}"
+ .strn "なかけﾞてﾞ相手をつ{换行}"
+ .strn "つみこんてﾞ攻撃す{换行}"
+ .strn "る｡{结束}{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2 :: Block0x1FA13A_003:
+;－－－－－－－－－－－－
+;.strn "ウォーティ{换行}"
+;.strn "　４大けﾞんそのひ{换行}"
+;.strn "とつ､水をつかさ{换行}"
+;.strn "とﾞる｡{换行}"
+;.strn "　はけﾞしい水てﾞ攻{换行}"
+;.strn "撃する｡{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "ウォーティ{换行}"
+ .strn "　４大けﾞんそのひ{换行}"
+ .strn "とつ､水をつかさ{换行}"
+ .strn "とﾞる｡{换行}"
+ .strn "　はけﾞしい水てﾞ攻{换行}"
+ .strn "撃する｡{结束}{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2 :: Block0x1FA13A_004:
+;－－－－－－－－－－－－
+;.strn "シﾞャンフﾟ{换行}"
+;.strn "　自分のかかとに{换行}"
+;.strn "はねをはやす｡敵{换行}"
+;.strn "の攻撃かﾞとてもよ{换行}"
+;.strn "けやすくなる｡{换行}"
+;.strn "　５ターンのみ｡{结束}"
+;－－－－－－－－－－－－
+ .strn "シﾞャンフﾟ{换行}"
+ .strn "　自分のかかとに{换行}"
+ .strn "はねをはやす｡敵{换行}"
+ .strn "の攻撃かﾞとてもよ{换行}"
+ .strn "けやすくなる｡{换行}"
+ .strn "　５ターンのみ｡{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2 :: Block0x1FA13A_005:
+;－－－－－－－－－－－－
+;.strn "サンタﾞー{换行}"
+;.strn "　はけﾞしい雷撃てﾞ{换行}"
+;.strn "攻撃し相手を１{换行}"
+;.strn "ターンしひﾞれさせ{换行}"
+;.strn "ることかﾞてﾞきる｡{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "サンタﾞー{换行}"
+ .strn "　はけﾞしい雷撃てﾞ{换行}"
+ .strn "攻撃し相手を１{换行}"
+ .strn "ターンしひﾞれさせ{换行}"
+ .strn "ることかﾞてﾞきる｡{结束}{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2 :: Block0x1FA13A_006:
+;－－－－－－－－－－－－
+;.strn "ソートﾞ{换行}"
+;.strn "　魔法のつえを剣{换行}"
+;.strn "にかえ､かれいな{换行}"
+;.strn "剣さはﾞきてﾞ相手を{换行}"
+;.strn "攻撃する｡{结束}"
+;－－－－－－－－－－－－
+ .strn "ソートﾞ{换行}"
+ .strn "　魔法のつえを剣{换行}"
+ .strn "にかえ､かれいな{换行}"
+ .strn "剣さはﾞきてﾞ相手を{换行}"
+ .strn "攻撃する｡{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2 :: Block0x1FA13A_007:
+;－－－－－－－－－－－－
+;.strn "シールトﾞ{换行}"
+;.strn "　相手のすへﾞての{换行}"
+;.strn "攻撃をふせきﾞ､タﾞ{换行}"
+;.strn "メーシﾞを２分の１{换行}"
+;.strn "にする｡{换行}"
+;.strn "　５ターンのみ｡{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "シールトﾞ{换行}"
+ .strn "　相手のすへﾞての{换行}"
+ .strn "攻撃をふせきﾞ､タﾞ{换行}"
+ .strn "メーシﾞを２分の１{换行}"
+ .strn "にする｡{换行}"
+ .strn "　５ターンのみ｡{结束}{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2 :: Block0x1FA13A_008:
+;－－－－－－－－－－－－
+;.strn "タイム{换行}"
+;.strn "　自分いかﾞいの時{换行}"
+;.strn "間の流れを止める｡{换行}"
+;.strn "　最初に４０Ｐ､{换行}"
+;.strn "いしﾞに３０Ｐの魔{换行}"
+;.strn "力を使う｡{结束}"
+;－－－－－－－－－－－－
+ .strn "タイム{换行}"
+ .strn "　自分いかﾞいの時{换行}"
+ .strn "間の流れを止める｡{换行}"
+ .strn "　最初に４０Ｐ､{换行}"
+ .strn "いしﾞに３０Ｐの魔{换行}"
+ .strn "力を使う｡{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2 :: Block0x1FA13A_009:
+;－－－－－－－－－－－－
+;.strn "ストーム{换行}"
+;.strn "　きょたﾞいなたつ{换行}"
+;.strn "まきてﾞ相手を攻撃{换行}"
+;.strn "する｡{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "ストーム{换行}"
+ .strn "　きょたﾞいなたつ{换行}"
+ .strn "まきてﾞ相手を攻撃{换行}"
+ .strn "する｡{结束}{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2 :: Block0x1FA13A_010:
+;－－－－－－－－－－－－
+;.strn "ファイト{换行}"
+;.strn "　とうしをもやし､{换行}"
+;.strn "体力とはやさかﾞ少{换行}"
+;.strn "しすﾞつしﾞょうしょ{换行}"
+;.strn "うする｡{换行}"
+;.strn "　１０ターンのみ{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "ファイト{换行}"
+ .strn "　とうしをもやし､{换行}"
+ .strn "体力とはやさかﾞ少{换行}"
+ .strn "しすﾞつしﾞょうしょ{换行}"
+ .strn "うする｡{换行}"
+ .strn "　１０ターンのみ{结束}{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2 :: Block0x1FA13A_011:
+;－－－－－－－－－－－－
+;.strn "スリーフﾟ{换行}"
+;.strn "　ねむりのこなを{换行}"
+;.strn "まき､相手をねむ{换行}"
+;.strn "らせる｡一定のか{换行}"
+;.strn "くりつてﾞ相手はね{换行}"
+;.strn "むりつつﾞける｡{结束}"
+;－－－－－－－－－－－－
+ .strn "スリーフﾟ{换行}"
+ .strn "　ねむりのこなを{换行}"
+ .strn "まき､相手をねむ{换行}"
+ .strn "らせる｡一定のか{换行}"
+ .strn "くりつてﾞ相手はね{换行}"
+ .strn "むりつつﾞける｡{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2 :: Block0x1FA13A_012:
+;－－－－－－－－－－－－
+;.strn "リターン{换行}"
+;.strn "　時間の流れをきﾞゃ{换行}"
+;.strn "くむきにし､１タ{换行}"
+;.strn "ーンかこにもとﾞす｡{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "リターン{换行}"
+ .strn "　時間の流れをきﾞゃ{换行}"
+ .strn "くむきにし､１タ{换行}"
+ .strn "ーンかこにもとﾞす｡{结束}{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2 :: Block0x1FA13A_013:
+;－－－－－－－－－－－－
+;.strn "ショット{换行}"
+;.strn "　すへﾞてをきりさ{换行}"
+;.strn "く風のやいはﾞてﾞ相{换行}"
+;.strn "手を攻撃する｡{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "ショット{换行}"
+ .strn "　すへﾞてをきりさ{换行}"
+ .strn "く風のやいはﾞてﾞ相{换行}"
+ .strn "手を攻撃する｡{结束}{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2 :: Block0x1FA13A_014:
+;－－－－－－－－－－－－
+;.strn "タﾞッシュ{换行}"
+;.strn "　体かﾞかるくなり､{换行}"
+;.strn "はやさかﾞ大きく{换行}"
+;.strn "しﾞょうしょうする｡{换行}"
+;.strn "　５ターンのみ｡{结束}"
+;－－－－－－－－－－－－
+ .strn "タﾞッシュ{换行}"
+ .strn "　体かﾞかるくなり､{换行}"
+ .strn "はやさかﾞ大きく{换行}"
+ .strn "しﾞょうしょうする｡{换行}"
+ .strn "　５ターンのみ｡{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2 :: Block0x1FA13A_015:
+;－－－－－－－－－－－－
+;.strn "フリースﾞ{换行}"
+;.strn "　相手をこおらせ{换行}"
+;.strn "る､きょたﾞいなこ{换行}"
+;.strn "おりのかたまりてﾞ{换行}"
+;.strn "攻撃する｡{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "フリースﾞ{换行}"
+ .strn "　相手をこおらせ{换行}"
+ .strn "る､きょたﾞいなこ{换行}"
+ .strn "おりのかたまりてﾞ{换行}"
+ .strn "攻撃する｡{结束}{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2 :: Block0x1FA13A_016:
+;－－－－－－－－－－－－
+;.strn "ファイアリー{换行}"
+;.strn "　４大けﾞんそのひ{换行}"
+;.strn "とつ､火をつかさ{换行}"
+;.strn "とﾞる｡はけﾞしいほ{换行}"
+;.strn "のおてﾞ相手をやき{换行}"
+;.strn "つくす｡{结束}"
+;－－－－－－－－－－－－
+ .strn "ファイアリー{换行}"
+ .strn "　４大けﾞんそのひ{换行}"
+ .strn "とつ､火をつかさ{换行}"
+ .strn "とﾞる｡はけﾞしいほ{换行}"
+ .strn "のおてﾞ相手をやき{换行}"
+ .strn "つくす｡{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2 :: Block0x1FA13A_017:
+;－－－－－－－－－－－－
+;.strn "スノウ{换行}"
+;.strn "　あたりを雪てﾞう{换行}"
+;.strn "もらせるふふﾞきてﾞ{换行}"
+;.strn "攻撃する｡{结束}"
+;－－－－－－－－－－－－
+ .strn "スノウ{换行}"
+ .strn "　あたりを雪てﾞう{换行}"
+ .strn "もらせるふふﾞきてﾞ{换行}"
+ .strn "攻撃する｡{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2 :: Block0x1FA13A_018:
+;－－－－－－－－－－－－
+;.strn "クラウトﾞ{换行}"
+;.strn "　空をくもてﾞおお{换行}"
+;.strn "い､サンタﾞーとス{换行}"
+;.strn "ノウの攻撃力かﾞ{换行}"
+;.strn "しﾞょうしょうする｡{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "クラウトﾞ{换行}"
+ .strn "　空をくもてﾞおお{换行}"
+ .strn "い､サンタﾞーとス{换行}"
+ .strn "ノウの攻撃力かﾞ{换行}"
+ .strn "しﾞょうしょうする｡{结束}{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2 :: Block0x1FA13A_019:
+;－－－－－－－－－－－－
+;.strn "サントﾞ{换行}"
+;.strn "　はけﾞしいすなあ{换行}"
+;.strn "らしてﾞ相手を攻撃{换行}"
+;.strn "する｡{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "サントﾞ{换行}"
+ .strn "　はけﾞしいすなあ{换行}"
+ .strn "らしてﾞ相手を攻撃{换行}"
+ .strn "する｡{结束}{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2 :: Block0x1FA13A_020:
+;－－－－－－－－－－－－
+;.strn "ツイン{换行}"
+;.strn "　３ターンのあいたﾞ､{换行}"
+;.strn "２回攻撃かﾞてﾞきる{换行}"
+;.strn "ようにかﾞ､まもるこ{换行}"
+;.strn "とかﾞてﾞきなくなる｡{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "ツイン{换行}"
+ .strn "　３ターンのあいたﾞ､{换行}"
+ .strn "２回攻撃かﾞてﾞきる{换行}"
+ .strn "ようにかﾞ､まもるこ{换行}"
+ .strn "とかﾞてﾞきなくなる｡{结束}{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2 :: Block0x1FA13A_021:
+;－－－－－－－－－－－－
+;.strn "アーシー{换行}"
+;.strn "　４大けﾞんそのひ{换行}"
+;.strn "とつ､地をつかさ{换行}"
+;.strn "とﾞる｡{换行}"
+;.strn "　大しﾞしんてﾞ攻撃{换行}"
+;.strn "する｡{结束}"
+;－－－－－－－－－－－－
+ .strn "アーシー{换行}"
+ .strn "　４大けﾞんそのひ{换行}"
+ .strn "とつ､地をつかさ{换行}"
+ .strn "とﾞる｡{换行}"
+ .strn "　大しﾞしんてﾞ攻撃{换行}"
+ .strn "する｡{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2 :: Block0x1FA13A_022:
+;－－－－－－－－－－－－
+;.strn "ハﾟワー{换行}"
+;.strn "　体中に力かﾞみな{换行}"
+;.strn "きﾞり､体力かﾞ大き{换行}"
+;.strn "くしﾞょうしょうす{换行}"
+;.strn "る｡{换行}"
+;.strn "　５ターンのみ｡{结束}{结束}"
+;－－－－－－－－－－－－
+ .strn "ハﾟワー{换行}"
+ .strn "　体中に力かﾞみな{换行}"
+ .strn "きﾞり､体力かﾞ大き{换行}"
+ .strn "くしﾞょうしょうす{换行}"
+ .strn "る｡{换行}"
+ .strn "　５ターンのみ｡{结束}{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2 :: Block0x1FA13A_023:
+;－－－－－－－－－－－－
+;.strn "フラワー{换行}"
+;.strn "　かれんな花々かﾞ{换行}"
+;.strn "体中のきすﾞをふさ{换行}"
+;.strn "きﾞ､きすﾞをかいふく{换行}"
+;.strn "する｡{结束}"
+;－－－－－－－－－－－－
+ .strn "フラワー{换行}"
+ .strn "　かれんな花々かﾞ{换行}"
+ .strn "体中のきすﾞをふさ{换行}"
+ .strn "きﾞ､きすﾞをかいふく{换行}"
+ .strn "する｡{结束}"
+;－－－－－－－－－－－－
+
+
+.align 2 :: Block0x1FA13A_024:
+;－－－－－－－－－－－－
+;.strn "アロー{换行}"
+;.strn "　料かﾞないのてﾞ{换行}"
+;.strn "ちょっとまってく{换行}"
+;.strn "たﾞさい｡{结束}"
+;－－－－－－－－－－－－
+ .strn "アロー{换行}"
+ .strn "　料かﾞないのてﾞ{换行}"
+ .strn "ちょっとまってく{换行}"
+ .strn "たﾞさい｡{结束}"
+;－－－－－－－－－－－－
+
+.endarea
+;子文件文本指针
+.org 0x1FA13A   ;暂勿改动指针所处地址
+;.align 2 :: Block0x1FA13A_Ptr:
+.area 0x1FA19E-.        ;原位覆写须限定area
+        .hword (Block0x1FA13A_000 & 0xF),(((Block0x1FA13A_000 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FA13A_001 & 0xF),(((Block0x1FA13A_001 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FA13A_002 & 0xF),(((Block0x1FA13A_002 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FA13A_003 & 0xF),(((Block0x1FA13A_003 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FA13A_004 & 0xF),(((Block0x1FA13A_004 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FA13A_005 & 0xF),(((Block0x1FA13A_005 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FA13A_006 & 0xF),(((Block0x1FA13A_006 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FA13A_007 & 0xF),(((Block0x1FA13A_007 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FA13A_008 & 0xF),(((Block0x1FA13A_008 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FA13A_009 & 0xF),(((Block0x1FA13A_009 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FA13A_010 & 0xF),(((Block0x1FA13A_010 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FA13A_011 & 0xF),(((Block0x1FA13A_011 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FA13A_012 & 0xF),(((Block0x1FA13A_012 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FA13A_013 & 0xF),(((Block0x1FA13A_013 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FA13A_014 & 0xF),(((Block0x1FA13A_014 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FA13A_015 & 0xF),(((Block0x1FA13A_015 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FA13A_016 & 0xF),(((Block0x1FA13A_016 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FA13A_017 & 0xF),(((Block0x1FA13A_017 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FA13A_018 & 0xF),(((Block0x1FA13A_018 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FA13A_019 & 0xF),(((Block0x1FA13A_019 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FA13A_020 & 0xF),(((Block0x1FA13A_020 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FA13A_021 & 0xF),(((Block0x1FA13A_021 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FA13A_022 & 0xF),(((Block0x1FA13A_022 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FA13A_023 & 0xF),(((Block0x1FA13A_023 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+        .hword (Block0x1FA13A_024 & 0xF),(((Block0x1FA13A_024 & 0xFFF0)>>4) + OffsetOfBlock0x1fInRamSegment)
+.endarea
+
+
+.func Block0x1FA13A_End :: .endfunc
